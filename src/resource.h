e18d783c629d5be1c5fcf6420ed54564be5e1fb6
Sergei Menchenin <sergei.menchenin@crossmatch.com>
2019-07-11 13:13:47 -0700

    Main window name change

--- src/resource.h
+++ src/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by UsingProSDK.rc
+// Used by UsingDigitalPersonaSDK.rc
 //
 #define IDS_APP_TITLE                   103
 #define IDC_USINGPROSDK                 109
